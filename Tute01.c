bc770e80964ab82ec97d1d9ff11a8000f76d5a0a
it21189876 <99862785+IT21189876@users.noreply.github.com>
2022-03-07 09:41:43 +0000

    Tutorail 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+  int m1, m2, sum;
+  float avg;
+
+  printf("enter subject 1 : ");
+  scanf("%d",&m1);
+
+  printf("enter subject 2 : ");
+  scanf("%d",&m2);
+
+  sum = m1 + m2;
+  avg = sum / 2.0;
+
+  printf("Avarage is %.2f",avg);
+  
   
   return 0;
 }
